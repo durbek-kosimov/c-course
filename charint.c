a0ffe238815dff2f212f195a20faaf3aae5eefc8
dkmint <dkosimov@gmail.com>
2020-04-01 00:36:18 +0200

    Add files via upload

---
+++ charint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+//    char c;
+    int i;
+//    i = 254;
+//    printf("Enter a character: ");
+    printf("Enter an integer: ");
+    scanf("%d", &i);
+//    scanf("%c", &c);
+//    printf("Integer value of %c = %d\n", c, c);
+    printf("Character value of %d = %c\n", i, i);
+    return 0;
+}
