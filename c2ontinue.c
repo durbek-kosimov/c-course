06d6b94cf295a4767e90a1a4d4c2560b2ec90d15
dkmint <dkosimov@gmail.com>
2020-04-13 19:47:18 +0200

    Add files via upload

--- c2ontinue.c
+++ c2ontinue.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
+#include <math.h>
+//int i;
 int main()
 {
-    int a = 0;
-    while (a < 5) {
-        int b = 0;
-        while (b < a) {
-            b ++;
+    int i = 0;
+    while (i <= 10) {
+        if (i == 5)
             continue;
-            printf("%d ", b);
-//            break;
-        }
-        a ++;
-//        break;
-//        continue;
-        printf("\n");
+        printf("%.f\n", pow(2, i));
+        i ++;
     }
-//    printf("%d\n", a);
-    printf("%d\n", b);
     return 0;
 }
