189c01a8eaf519d046fd578266afd541155e2cd3
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2020-11-30 22:51:20 +0100

    Add files via upload
    The Character Code of ABC

---
+++ alpha.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char ch = 'A';
+    printf("The character %c has the character code %d.\n", ch, ch);
+    for (; ch <= 'Z'; ++ch)
+        printf("%2c", ch);
+    printf("\n");
+    return 0;
+}
