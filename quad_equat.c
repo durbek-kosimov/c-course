4c8316902f8194e49fb88b6d5ed683226ef8d837
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2021-01-25 23:07:56 +0100

    Quadratiq Equation

--- quad_equat.c
+++ quad_equat.c
@@ -19,10 +19,6 @@ int main()
         d = b*b - 4*a*c;
         if(d < 0)
             printf("Equation does not have solutions\n");
-        else if (d = 0) {
-            x1 = -b/2*a;
-            printf("x1 = x2 = %3.f\n", x1);
-        }   
         else {
             x1 = (-b + sqrt(d)) / (2*a);
             x2 = (-b - sqrt(d)) / (2*a);
